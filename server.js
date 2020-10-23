dd2e4a1bef91e79ffd042ee049c5cdfeb25f0d6e
Priya Reddy <saipriyareddy94@gmail.com>
2020-10-23 15:14:58 -0400

    Added token expiration logic

--- server.js
+++ server.js
@@ -5,22 +5,21 @@ const path = require('path');
 const jwt = require('jsonwebtoken');
 const exjwt = require('express-jwt');
 const bodyParser = require('body-parser');
-
-app.use((req,res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
-    next();
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( { extended:true }));
-
 const axios = require('axios');
 const PORT = 3000;
 const secretKey = 'My super secret key';
 const jwtMW = exjwt({
     secret: secretKey,
     algorithms: ['HS256']
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended:true }));
+
+app.use((req,res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+    next();
 });
 
 let users = [
@@ -49,7 +48,7 @@ app.post('/api/login', (req, res) => {
 
     for(let user of users) {
         if(username == user.username && password == user.password) {
-            let token = jwt.sign({ id: user.id, username: user.username}, secretKey, {expiresIn: 60 * 3});
+            let token = jwt.sign({ id: user.id, username: user.username}, secretKey, {expiresIn: 60 * 3 });
             res.json({
                 success: true,
                 err: null,
@@ -65,6 +64,7 @@ app.post('/api/login', (req, res) => {
             err: 'Username or Password is incorrect'
         });
     } 
+      
 });
 
 app.get('/api/dashboard', jwtMW, (req, res) => {
@@ -72,24 +72,26 @@ app.get('/api/dashboard', jwtMW, (req, res) => {
         success: true,
         myContent: 'Secret content that only logged in people can see'
     });
-})
+});
 
 app.get('/api/settings', jwtMW, (req, res) => {
     res.json({
         success: true,
         myContent: 'Only login person with valid token can see this page'
     });
-})
+});
 
 app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.use(function (err, req, res, next) {
+
   if(err.name === 'UnauthorizedError') {
-      if(err.inner.name == 'TokenExpiredError') {
-          axios.get('http:localhost:3000');
-          return;
+    if(err.inner.name == 'TokenExpiredError') {
+        console.log(err.inner.name);
+        axios.get('http://localhost:3000');
+        return;
       }
       res.status(401).json({
               success: false,
