0c130765c6c96c36f4efe9f7b0660fb14aba8d08
Priya Reddy <saipriyareddy94@gmail.com>
2020-10-23 21:18:11 -0400

    Added logic to handle expire token

--- server.js
+++ server.js
@@ -88,11 +88,11 @@ app.get('/', (req, res) => {
 app.use(function (err, req, res, next) {
 
   if(err.name === 'UnauthorizedError') {
-    if(err.inner.name == 'TokenExpiredError') {
-        console.log(err.inner.name);
-        axios.get('http://localhost:3000');
-        return;
-      }
+    // if(err.inner.name == 'TokenExpiredError') {
+    //     console.log(err.inner.name);
+    //     axios.get('http://localhost:3000');
+    //     return;
+    //   }
       res.status(401).json({
               success: false,
               officialError: err,
